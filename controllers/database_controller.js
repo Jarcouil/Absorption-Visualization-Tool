2fc3ade8571db7ace117d61bc22313a13cc9291a
Jarco Uil <33051675+Jarcouil@users.noreply.github.com>
2021-05-18 10:59:31 +0200

    Remove legacy code

--- controllers/database_controller.js
+++ controllers/database_controller.js
@@ -2,7 +2,6 @@ var sql_connection = require('../database_connect');
 
 module.exports = {
     execute_sql,
-    insert_data,
     insert_file_sql,
 }
 
@@ -23,23 +22,6 @@ function execute_sql(sql, data = null) {
                 }
             }
         );
-    })
-}
-
-function insert_data(sql, data) {
-    sql = getInsertdataSQL(data)
-    return new Promise(function (resolve, reject) {
-        if (err) { reject(err); }
-
-        sql_connection.query(sql, data, function (err, result) {
-            if (err) {
-                sql_connection.rollback(function () {
-                    reject(err);
-                    return;
-                });
-            }
-        })
-
     })
 }
 
@@ -84,19 +66,6 @@ function insert_file_sql(sql, data) {
     })
 }
 
-function getInsertdataSQL(data) {
-    var sql = "INSERT INTO `" + data.shift() + "` VALUES "
-
-    data.forEach(element => {
-        var row = ""
-        element.forEach(element2 => {
-            row = row + ", `" + element2.toString() + "` "
-        })
-        sql = sql + "(" + row + "),"
-    })
-    return sql.slice(0, -1) + ";"
-}
-
 function getAlterTableSQL(data) {
     var sql = "ALTER TABLE `" + data.shift() + "`"
     data.forEach(element => {
@@ -104,4 +73,3 @@ function getAlterTableSQL(data) {
     });
     return sql.slice(0, -1) + ";"
 }
-
