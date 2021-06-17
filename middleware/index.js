a375ac6a7f521acd526749b2015757ec1da9d63a
Jarco Uil <33051675+Jarcouil@users.noreply.github.com>
2021-06-17 13:14:18 +0200

    update middleware index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,13 @@
 const authJwt = require("./authJwt");
 const verifySignUp = require("./verifySignUp");
 const verifyFile = require("./verifyFile");
+const verifyLogin = require("./verifyLogin");
 const verifyMeasurement = require("./verifyMeasurement");
 
 module.exports = {
   authJwt,
   verifySignUp,
   verifyFile,
-  verifyMeasurement
+  verifyMeasurement,
+  verifyLogin
 };
